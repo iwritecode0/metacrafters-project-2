977c22af722fbbca064f429ea5a65f3d2a3343f2
iwritecode0 <apkfrom42@gmail.com>
2024-07-19 19:26:17 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "hrp"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "hchain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "HC"
 )
 
 var ID ids.ID
